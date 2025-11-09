67464fb5aacc6cfeef3bcaa42bee64c74c984c50
Khalid Talakshi <khalid.talakshi@outlook.com>
2025-11-09 18:16:22 -0500

    setup tabs

--- main.py
+++ main.py
@@ -1,13 +1,45 @@
-import numpy as np
-import pandas as pd
-import streamlit as st
 import datetime as dt
+
+import streamlit as st
 from fastf1 import get_event_schedule
-from utils import type_to_options, get_session_data
+
+from utils import get_info_table, get_session_data, type_to_options
+
+race_tabs = ["Race Results", "Driver Lap Times"]
+qualifying_tabs = ["Race Results", "Driver Lap Times"]
+practice_tabs = ["Driver Lap Times"]
+
+
+def generate_qualifying_tabs(session):
+    tab1, tab2 = st.tabs(qualifying_tabs)
+    with tab1:
+        st.subheader("Qualifying Results")
+        st.write(get_info_table(session, "Qualifying").reset_index(drop=True))
+    with tab2:
+        st.subheader("Driver Lap Times")
+
+
+def generate_race_tabs(session):
+    tab1, tab2 = st.tabs(race_tabs)
+    with tab1:
+        st.subheader("Race Results")
+        st.write(get_info_table(session, "Race").reset_index(drop=True))
+    with tab2:
+        st.subheader("Driver Lap Times")
+
+
+def generate_main_view(event, session, session_option):
+    st.subheader(f"{event['EventName']} - {session.session_info['Name']} Leaderboard")
+    if session_option == "Race":
+        generate_race_tabs(session)
+    elif session_option == "Qualifying":
+        generate_qualifying_tabs(session)
 
 
 def main():
     st.sidebar.title("Pit Wall")
+    st.title("Pit Wall Analysis")
+
     year = st.sidebar.selectbox(
         "Year",
         options=reversed([i for i in range(2018, dt.datetime.now().year + 1)]),
@@ -30,16 +62,11 @@ def main():
             index=None,
             placeholder="Select Event",
         )
-        st.title(f"Pit Wall Analysis for {year}")
     else:
-        st.title("Pit Wall Analysis")
         st.subheader("Select a year from the sidebar to view data.")
 
     if round_select is not None:
         event = schedule.get_event_by_round(round_select)
-        st.write(
-            f"Selected Year: {year}, Selected Round: {event['EventName']} (Round {event['RoundNumber'].item()})"
-        )
         session_select = st.sidebar.selectbox(
             "Session",
             options=type_to_options(event["EventFormat"]),
@@ -50,11 +77,7 @@ def main():
     if session_select is not None:
         session = get_session_data(event, session_select)
         session.load()
-        if session is not None:
-            st.subheader(f"{session_select} Data")
-            st.dataframe(session.results)
-        else:
-            st.write("Session data not available.")
+        generate_main_view(event, session, session_select)
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -1,9 +1,14 @@
+import numpy as np
+import pandas as pd
+
 from constants import (
     CONVENTIONAL_OPTIONS,
     SPRINT_OPTIONS,
     SPRINT_QUALIFYING_OPTIONS,
     SPRINT_SHOOTOUT_OPTIONS,
 )
+
+
 def type_to_options(event_type: str) -> list:
     if event_type == "conventional":
         return CONVENTIONAL_OPTIONS
@@ -34,5 +39,41 @@ def get_session_data(event, session_option: str):
         return event.get_sprint_shootout()
     elif session_option == "Race":
         return event.get_race()
+    else:
+        return None
+
+
+def get_info_table(session, session_option):
+    RACE_KEYS = [
+        "ClassifiedPosition",
+        "Points",
+        "FullName",
+        "TeamName",
+        "GridPosition",
+        "Laps",
+        "Status",
+    ]
+
+    QUALIFYING_KEYS = [
+        "Position",
+        "FullName",
+        "TeamName",
+        "Q1",
+        "Q2",
+        "Q3",
+    ]
+    session.results["Q1"] = session.results["Q1"].apply(
+        lambda x: x.total_seconds() if pd.notnull(x) else np.nan
+    )
+    session.results["Q2"] = session.results["Q2"].apply(
+        lambda x: x.total_seconds() if pd.notnull(x) else np.nan
+    )
+    session.results["Q3"] = session.results["Q3"].apply(
+        lambda x: x.total_seconds() if pd.notnull(x) else np.nan
+    )
+    if session_option == "Race":
+        return session.results[RACE_KEYS]
+    elif session_option == "Qualifying":
+        return session.results[QUALIFYING_KEYS]
     else:
         return None
